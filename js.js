23d17e710ece9eb53588c7c4c3b646d431c55573
Vitalik2607 <vitalbmx2607@gmail.com>
2020-04-05 18:38:00 +0300

    add

---
+++ js.js
@@ -0,0 +1,7 @@
+let alcohol = +prompt("Виктор, введите содержание спирта");
+let volume = +prompt("Ведите объем жидкости в литрах ");
+let mililitres = volume * 1000;
+let newAlcohol = mililitres / 100 ;
+let absoluteAlcohol = newAlcohol *  alcohol;
+let litres = absoluteAlcohol / 1000;
+alert("Содержание абсолютного спирта " + litres+"л")
